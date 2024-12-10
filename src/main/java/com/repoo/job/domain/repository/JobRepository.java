90b1bdc7d26b5078229d74ae6de9db19b1a538c3
이재현 <2023050@bssm.hs.kr>
2024-12-11 04:18:17 +0900

    fix(jobpost): Repository 어노테이션 추가

--- src/main/java/com/repoo/job/domain/repository/JobRepository.java
+++ src/main/java/com/repoo/job/domain/repository/JobRepository.java
@@ -2,6 +2,8 @@ package com.repoo.job.domain.repository;
 
 import com.repoo.job.domain.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
 }
