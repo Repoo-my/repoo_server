5d0bc0dca604f3660d22ebac56c35fb401341c30
이재현 <2023050@bssm.hs.kr>
2024-12-11 04:18:36 +0900

    fix(user): user 컬럼 추가

--- src/main/java/com/repoo/user/domain/Users.java
+++ src/main/java/com/repoo/user/domain/Users.java
@@ -14,5 +14,17 @@ public class Users {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long users_id;
+    private Long usersId;
+
+    private String userName;
+
+    private String userPassword;
+
+    private String userEmail;
+
+    private String userGender;
+
+    private String userAge;
+
+    private String profileImg;
 }
