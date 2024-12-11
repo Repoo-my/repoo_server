8ac79f6a6f8a65e47823d29ba8bd2ee9338caa69
이재현 <2023050@bssm.hs.kr>
2024-12-11 11:54:08 +0900

    add(curriculumVitae): 학력 통합 서비스 추가

--- src/main/java/com/repoo/education/service/QueryEducationService.java
+++ src/main/java/com/repoo/education/service/QueryEducationService.java
@@ -1,9 +1,30 @@
 package com.repoo.education.service;
 
+import com.repoo.curriculumvitae.domain.CurriculumVitae;
+import com.repoo.curriculumvitae.service.implementation.CurriculumVitaeReader;
+import com.repoo.education.domain.Education;
+import com.repoo.education.service.implementation.EducationReader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class QueryEducationService {
+
+    private final EducationReader educationReader;
+    private final CurriculumVitaeReader curriculumVitaeReader;
+
+    public Education getEducation(Long id) {
+        return educationReader.getEducation(id);
+    }
+
+    public List<Education> getEducations() {
+        return educationReader.getEducations();
+    }
+
+    public List<Education> getEducationsByCurriculumVitae(Long curriculumVitaeId) {
+        return educationReader.getEducationsByCurriculumVitae(curriculumVitaeReader.getCurriculumVitae(curriculumVitaeId));
+    }
 }
