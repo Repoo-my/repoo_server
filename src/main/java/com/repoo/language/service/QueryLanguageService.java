d0577a9d5ded28fb4921cd2c9ed795b2f7efcb08
이재현 <2023050@bssm.hs.kr>
2024-12-11 17:34:16 +0900

    fix(): 변수명 맞추기

--- src/main/java/com/repoo/language/service/QueryLanguageService.java
+++ src/main/java/com/repoo/language/service/QueryLanguageService.java
@@ -1,4 +1,31 @@
 package com.repoo.language.service;
 
+import com.repoo.curriculumvitae.domain.CurriculumVitae;
+import com.repoo.curriculumvitae.service.implementation.CurriculumVitaeReader;
+import com.repoo.language.domain.Language;
+import com.repoo.language.service.implementation.LanguageReader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class QueryLanguageService {
+
+    private final LanguageReader languageReader;
+    private final CurriculumVitaeReader curriculumVitaeReader;
+
+    public Language getLanguage(Long id) {
+        return languageReader.findById(id);
+    }
+
+    public List<Language> getLanguages() {
+        return languageReader.findAll();
+    }
+
+    public List<Language> getLanguagesByCurriculumVitae(Long curriculumVitaeId) {
+        return languageReader.findByCurriculumVitaeId(curriculumVitaeReader.getCurriculumVitae(curriculumVitaeId));
+    }
+
 }
