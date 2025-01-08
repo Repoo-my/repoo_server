4222e2bdd3c64ef6927307e428d52dd0f9ead46a
이재현 <2023050@bssm.hs.kr>
2025-01-08 13:27:28 +0900

    fix(): 공고 업데이트 에러 해결

--- src/main/java/com/repoo/domain/main/jobpost/service/implementation/JobPostUpdater.java
+++ src/main/java/com/repoo/domain/main/jobpost/service/implementation/JobPostUpdater.java
@@ -1,10 +1,15 @@
 package com.repoo.domain.main.jobpost.service.implementation;
 
 import com.repoo.domain.main.jobpost.domain.JobPost;
+import com.repoo.domain.main.jobpost.domain.repository.JobPostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class JobPostUpdater {
+
+    private final JobPostRepository jobPostRepository;
 
     public void update(JobPost updatableJobPost, JobPost jobPost) {
         updatableJobPost.update(
@@ -15,5 +20,7 @@ public class JobPostUpdater {
                 jobPost.getTitle(),
                 jobPost.getContents(),
                 jobPost.getJobpostImg());
+
+        jobPostRepository.save(jobPost);
     }
 }
