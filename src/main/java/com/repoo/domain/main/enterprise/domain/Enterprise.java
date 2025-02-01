9c8b72235c15cc468f4b33dab57dfb928555ec0d
ljhy07 <ij184399@gmail.com>
2025-02-01 23:06:39 +0900

    fix(기업): 전에 추가하지 않은 컬럼들 추가

--- src/main/java/com/repoo/domain/main/enterprise/domain/Enterprise.java
+++ src/main/java/com/repoo/domain/main/enterprise/domain/Enterprise.java
@@ -1,9 +1,11 @@
 package com.repoo.domain.main.enterprise.domain;
 
+import io.hypersistence.utils.hibernate.type.array.StringArrayType;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
@@ -16,9 +18,47 @@ public class Enterprise {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long enterpriseId;
 
-    private String EnterpriseName;
+    private String enterpriseName;
 
-    public void update(String enterpriseName){
-        this.EnterpriseName = EnterpriseName;
+    private String enterprisePassword;
+
+    private String enterpriseDescription;
+
+    private String enterpriseEmail;
+
+    private String enterprisePhone;
+
+    @Type(StringArrayType.class)
+    @Column(columnDefinition = "varchar[]")
+    private String[] enterpriseTags;
+
+//    private String enterpriseImg;
+
+    public Enterprise(String enterpriseName,
+                      String enterprisePassword,
+                      String enterpriseDescription,
+                      String enterpriseEmail,
+                      String enterprisePhone,
+                      String[] enterpriseTags) {
+        this.enterpriseName = enterpriseName;
+        this.enterprisePassword = enterprisePassword;
+        this.enterpriseDescription = enterpriseDescription;
+        this.enterpriseEmail = enterpriseEmail;
+        this.enterprisePhone = enterprisePhone;
+        this.enterpriseTags = enterpriseTags;
+    }
+
+    public void update(String enterpriseName,
+                       String enterprisePassword,
+                       String enterpriseDescription,
+                       String enterpriseEmail,
+                       String enterprisePhone,
+                       String[] enterpriseTags){
+        this.enterpriseName = enterpriseName;
+        this.enterprisePassword = enterprisePassword;
+        this.enterpriseDescription = enterpriseDescription;
+        this.enterpriseEmail = enterpriseEmail;
+        this.enterprisePhone = enterprisePhone;
+        this.enterpriseTags = enterpriseTags;
     }
 }
